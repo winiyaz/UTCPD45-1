b1f51c1cd9ccf886da9ca98dbd9a97418dc5c1e9
winiyaz <winiyaz@proton.me>
2024-09-17 09:14:05 +0000

    .

--- bs4/m1/v1.py
+++ bs4/m1/v1.py
@@ -2,12 +2,42 @@
 
 import requests
 from bs4 import BeautifulSoup
+from rich import print as rprint
 
-url = "https://www.mksecrets.net/index.php?section=mk4&lang=eng&contentID=5982&title=Mortal-Kombat-4"
-response = requests.get(url)
+# Defining a function to run the extraction and writing 
 
-soup = BeautifulSoup(response.content, 'html.parser')
+def ext1():
 
-# Write the HTML content to a file
-with open('panty.html', 'w') as f:
-    f.write(soup.prettify())
+    url = "https://www.mksecrets.net/index.php?section=mk4&lang=eng&contentID=5982&title=Mortal-Kombat-4"
+    response = requests.get(url)
+
+    mysoup = BeautifulSoup(response.content, 'html.parser')
+
+    # Write the HTML content to a file
+    with open('panty.html', 'w') as f:
+        f.write(mysoup.prettify())
+        
+    print('Extracted !!!')
+    
+    
+        
+def ext2():
+
+    url = "https://www.mksecrets.net/index.php?section=mk4&lang=eng&contentID=5982&title=Mortal-Kombat-4"
+    response = requests.get(url)
+
+    mysoup = BeautifulSoup(response.content, 'html.parser')
+    
+    # rprint(mysoup.prettify())
+    extracted_elements = mysoup.find_all(name='a')
+    # rprint(extracted_elements)
+    
+    headingz = [a.get('href') for a in mysoup.find_all(name='a')]
+    rprint(headingz)
+    
+    # for a in extracted_elements:
+    #     rprint(a.get("href"))
+    
+        
+ext2()
+
