6b18ea07d47e6a5cf05c3d437489c8a1124adccf
winiyaz <winiyaz@proton.me>
2024-09-17 07:22:50 +0000

    .

--- bs4/m2/v3.py
+++ bs4/m2/v3.py
@@ -10,7 +10,7 @@ response = requests.get("https://immunefi.com/bug-bounty/")
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # Extract data as needed
-data = soup.find_all('div', class_='desired-class')
+data = soup.find_all('table', class_='desired-class')
 for item in data:
     print(item.text)
 
