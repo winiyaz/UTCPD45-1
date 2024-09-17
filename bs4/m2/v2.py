89345690a617f6ed9bcb1755876c10a221177bd2
winiyaz <winiyaz@proton.me>
2024-09-17 07:13:02 +0000

    .

---
+++ bs4/m2/v2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+
+import requests
+from bs4 import BeautifulSoup
+
+# Send a GET request to the page
+response = requests.get("https://immunefi.com/bug-bounty/")
+
+# Parse with Beautiful Soup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract data as needed
+data = soup.find_all('div', class_='desired-class')
+for item in data:
+    print(item.text)
+
+# Write to HTML file
+with open('im.html', 'w') as f:
+    f.write(soup.prettify())
