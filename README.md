199b112b5c98bae45ec7653626a7af024896021a
winiyaz <winiyaz@proton.me>
2024-09-17 03:50:42 +0000

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UTCPD45-1
 UTCPD45-1PantyScratching
+
+> This is focused on WS 
---
+++ wz/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspace/aedryin-smellpanty/mx/pnp.fish
+/workspace/aedryin-smellpanty/mx/fixrs.fish
+/workspace/aedryin-smellpanty/mx/st.sh
+/workspace/aedryin-smellpanty/mx/aryt.fish
+/workspace/aedryin-smellpanty/mx/tgpt.fish
+/workspace/aedryin-smellpanty/mx/bun.fish
+/workspace/aedryin-smellpanty/mx/tmuxx.fish
+
+
